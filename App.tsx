bfc2d3197e1c292b3f9660655ecf71cff3243dcc
Gabriel Souza <gabrielss_@outlook.com>
2021-12-15 15:37:26 -0300

    Schedule Details Screen design

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import { ThemeProvider } from "styled-components";
 import theme from "./src/styles/theme";
 import { CarDetails } from "./src/screens/CarDetails";
 import { Schedule } from "./src/screens/Schedule";
+import { ScheduleDetails } from "./src/screens/ScheduleDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Schedule />
+      <ScheduleDetails />
     </ThemeProvider>
   );
 }
---
+++ src/screens/ScheduleDetails/index.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { Accessory } from "../../components/Accessory";
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+import { Feather } from "@expo/vector-icons";
+
+import {
+  Container,
+  SafeArea,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  Footer,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from "./styles";
+import { Button } from "../../components/Button";
+import { RFValue } from "react-native-responsive-fontsize";
+import { useTheme } from "styled-components";
+
+export function ScheduleDetails() {
+  const theme = useTheme();
+
+  return (
+    <SafeArea>
+      <Container>
+        <Header>
+          <BackButton onPress={() => {}} />
+        </Header>
+
+        <CarImages>
+          <ImageSlider
+            imagesUrl={[
+              "https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png",
+            ]}
+          />
+        </CarImages>
+
+        <Content>
+          <Details>
+            <Description>
+              <Brand>Lamborghini</Brand>
+              <Name>Huracan</Name>
+            </Description>
+
+            <Rent>
+              <Period>Ao dia</Period>
+              <Price>R$ 580</Price>
+            </Rent>
+          </Details>
+
+          <Accessories>
+            <Accessory name="380km/h" icon={SpeedSvg} />
+            <Accessory name="3.2s" icon={AccelerationSvg} />
+            <Accessory name="800 HP" icon={ForceSvg} />
+            <Accessory name="Gasolina" icon={GasolineSvg} />
+            <Accessory name="Auto" icon={ExchangeSvg} />
+            <Accessory name="2 pessoas" icon={PeopleSvg} />
+          </Accessories>
+
+          <RentalPeriod>
+            <CalendarIcon>
+              <Feather
+                name="calendar"
+                size={RFValue(24)}
+                color={theme.colors.background_secondary}
+              />
+            </CalendarIcon>
+
+            <DateInfo>
+              <DateTitle>De</DateTitle>
+              <DateValue>18/06/2021</DateValue>
+            </DateInfo>
+
+            <Feather
+              name="chevron-right"
+              size={RFValue(10)}
+              color={theme.colors.text}
+            />
+
+            <DateInfo>
+              <DateTitle>Até</DateTitle>
+              <DateValue>18/06/2021</DateValue>
+            </DateInfo>
+          </RentalPeriod>
+
+          <RentalPrice>
+            <RentalPriceLabel>Total</RentalPriceLabel>
+            <RentalPriceDetails>
+              <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+              <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+            </RentalPriceDetails>
+          </RentalPrice>
+        </Content>
+
+        <Footer>
+          <Button title="Alugar agora" color={theme.colors.success} />
+        </Footer>
+      </Container>
+    </SafeArea>
+  );
+}
